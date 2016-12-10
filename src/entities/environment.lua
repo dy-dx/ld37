c0512c02b92b071de740cd7c0ce2422402689fdd
Greg <gregt87@gmail.com>
2016-12-10 14:19:24 -0500

    added background

--- src/entities/environment.lua
+++ src/entities/environment.lua
@@ -1,21 +1,13 @@
 -- local gamestate = require "lib.gamestate"
 
 local Environment = Class{}
-local Panel = require 'entities/panel'
-
 
 function Environment:init()
-	self.isEnvironment = true
-	self.currentPanel = Panel()
+    self.pos = {x = 0, y = 0}
+    self.sprite = love.graphics.newImage("assets/images/spaceship_bridge_by_izacp-d9duao3.png")
 end
 
 function Environment:draw(dt)
-
-	love.graphics.setColor(255, 255, 0, 255)
-    love.graphics.rectangle('fill', 0, 0, 1000, 2000)
-    love.graphics.setColor(255, 255, 255)
-
-    self.currentPanel:draw(dt)
 end
 
 return Environment
