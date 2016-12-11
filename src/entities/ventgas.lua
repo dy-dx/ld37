026d3dc6d9888f6c5ec3c71bbd316878b224a221
Diane Losardo <dlosardo@gmail.com>
2016-12-10 19:00:50 -0500

    resize ventgas minigame

--- src/entities/ventgas.lua
+++ src/entities/ventgas.lua
@@ -1,55 +1,61 @@
 local VentGas = Class{}
-local screenWidth = love.graphics.getWidth()
-local screenHeight = love.graphics.getHeight()
 
 function VentGas:init()
+
+    local screenWidth = love.graphics.getWidth()
+    local screenHeight = love.graphics.getHeight()
+    local padding = 40
+    local barPadding = 20
+    local barWidth = 60
+    local buttonRadius = 30
+    local buttonYOffset = 40
+    local maxPressure = 300
+
     self.isDrawVentGasSystem = true
     self.isVentGasControlSystem = true
-    self.isVisible = true
-    self.x = 0
-    self.y = 0
-    self.width = screenWidth
-    self.height = screenHeight*2/3
+    self.x = padding
+    self.y = padding
+    self.width = screenWidth - 2 * padding
+    self.height = screenHeight - 2 * padding * 1.5
     self.r = 10
     self.g = 175
     self.b = 35
 
     -- gas pressure
     self.gasPressureDecrease = 100
-    self.gasGrowthRate = 1
-    self.maxGasPressure = 300
+    self.gasGrowthRate = .1
+    self.maxGasPressure = maxPressure
     self.currentGasPressure = 0
-    self.gasPressureBox = {x = screenWidth*1/4
-        , y = 20, width = 60
+    self.gasPressureBox = {x = self.width*1/4
+        , y = self.y + barPadding, width = barWidth
         , height = self.maxGasPressure, r=25, g=25, b=75
         }
-    self.gasPressureButton = {x=self.gasPressureBox.x + 30
-        , y=self.gasPressureBox.y + self.gasPressureBox.height + 40
-        , radius=30, r=0, g=255, b=40, text='Press Here'}
+    self.gasPressureButton = {x=self.gasPressureBox.x + buttonRadius
+        , y=self.gasPressureBox.y + self.gasPressureBox.height + buttonYOffset
+        , radius=buttonRadius, r=0, g=255, b=40, text='Press Here'}
 
     -- oxygen
-    self.maxOxygenPressure = 300
-    self.oxygenPressureBox = {x = screenWidth*2/4
-        , y = 20, width = 60
+    self.maxOxygenPressure = maxPressure
+    self.oxygenPressureBox = {x = self.width*2/4
+        , y = self.y + barPadding, width = barWidth
         , height = self.maxOxygenPressure, r=25, g=25, b=75
         }
-    self.oxygenPressureButton = {x=self.oxygenPressureBox.x + 30
-        , y=self.oxygenPressureBox.y + self.oxygenPressureBox.height + 40
-        , radius=30, r=0, g=255, b=40, text='DO NOT Press Here'}
+    self.oxygenPressureButton = {x=self.oxygenPressureBox.x + buttonRadius
+        , y=self.oxygenPressureBox.y + self.oxygenPressureBox.height + buttonYOffset
+        , radius=buttonRadius, r=0, g=255, b=40, text='DO NOT Press Here'}
 
     -- unknown pressure
     self.unknownPressureDecrease = 50
-    self.unknownGrowthRate = 1.5
-    self.maxUnknownPressure = 300
+    self.unknownGrowthRate = .2
+    self.maxUnknownPressure = maxPressure
     self.currentUnknownPressure = 0
-    self.unknownPressureBox = {x = screenWidth*3/4
-        , y = 20, width = 60
+    self.unknownPressureBox = {x = self.width*3/4
+        , y = self.y + barPadding, width = barWidth
         , height = self.maxUnknownPressure, r=25, g=25, b=75
         }
-    self.currentunknownPressure = 0
-    self.unknownPressureButton = {x=self.unknownPressureBox.x + 30
-        , y=self.unknownPressureBox.y + self.unknownPressureBox.height + 40
-        , radius=30, r=0, g=255, b=40, text='Press Here'}
+    self.unknownPressureButton = {x=self.unknownPressureBox.x + buttonRadius
+        , y=self.unknownPressureBox.y + self.unknownPressureBox.height + buttonYOffset
+        , radius=buttonRadius, r=0, g=255, b=40, text='Press Here'}
 end
 
 return VentGas
