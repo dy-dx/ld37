ed6a9610991afe90a7aed3bbccc646478c2d1c14
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2016-12-10 21:09:45 -0500

    Plumbing stuff.

--- src/entities/misslowcommand/player.lua
+++
@@ -1,9 +0,0 @@
--- local gamestate = require "lib.gamestate"
-
-local MisslowCommandPlayer = Class{}
-
-function MisslowCommandPlayer:init()
-    self.controllable = true
-end
-
-return MisslowCommandPlayer
