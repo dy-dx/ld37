8f9e60c8937804978878dc4fbe4f4f6d72e39607
Chris Kim <christopherjkim@gmail.com>
2016-12-11 15:13:11 -0500

    add bobbing anims and example alien animation

--- src/states/level0.lua
+++ src/states/level0.lua
@@ -5,6 +5,7 @@ local NavPanel = require 'entities/navpanel'
 local VentGas = require 'entities/ventgas'
 local DebugInfo = require 'entities/debuginfo'
 local Text = require 'entities/Text'
+local CrewMemberEngineer = require 'entities/crewmemberengineer'
 
 local Level = Class{}
 function Level:init()
@@ -70,6 +71,7 @@ function Level:load()
 
     world:addEntity(NavPanel())
     world:addEntity(VentGas())
+    world:addEntity(CrewMemberEngineer())
     world:addEntity(Panel({x = 80, y = 160, w = 160, h = 220}, "misslowcommand")) -- needs to take panel graphis
     world:addEntity(Panel({x = 560, y = 160, w = 160, h = 220}, "ventgas"))
     world:addEntity(Panel({x = 10, y = 300, w = 100, h = 250}, "spinner"))
