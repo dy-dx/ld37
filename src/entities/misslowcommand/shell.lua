30dac18b55c6029de9f36b371658c3972b2a8a8d
Brian Shaginaw <brian.shaginaw@warbyparker.com>
2016-12-11 20:23:43 -0500

    Hook up misslow command animations.

--- src/entities/misslowcommand/shell.lua
+++ src/entities/misslowcommand/shell.lua
@@ -16,18 +16,18 @@ function Shell:init(dest_x, dest_y)
 
     local offset_x = dest_x - start_x
     local offset_y = dest_y - start_y
-    angle = math.atan2(offset_y, offset_x)
+    self.rot = math.atan2(offset_y, offset_x)
 
     self.velocity = {
-        x = math.cos(angle) * Shell.SPEED,
-        y = math.sin(angle) * Shell.SPEED,
+        x = math.cos(self.rot) * Shell.SPEED,
+        y = math.sin(self.rot) * Shell.SPEED,
     }
     self.pos = {x = start_x, y = start_y}
-    self.sprite = love.graphics.newImage('assets/images/bomb_sheet.png')
+    self.sprite = love.graphics.newImage('assets/images/bomb2_sheet.png')
     self.target_sprite = love.graphics.newImage('assets/images/target.png')
 
     local g = anim8.newGrid(20, 20, self.sprite:getWidth(), self.sprite:getHeight())
-    self.animation = anim8.newAnimation(g('1-2', 1), 0.1)
+    self.animation = anim8.newAnimation(g(1, '1-2'), 0.1)
     self.offset = {
         x = 20 / 2,
         y = 20 / 2,
--- src/entities/misslowcommand/ship.lua
+++ src/entities/misslowcommand/ship.lua
@@ -1,4 +1,5 @@
 -- local gamestate = require "lib.gamestate"
+local anim8 = require '../../vendor/anim8'
 
 local Ship = Class{}
 start_x = 400
@@ -9,11 +10,13 @@ function Ship:init()
     self.isDead = false
 
     self.pos = {x = start_x, y = start_y}
-    self.sprite = love.graphics.newImage('assets/images/ship.png')
+    self.sprite = love.graphics.newImage('assets/images/ship_sheet.png')
 
+    local g = anim8.newGrid(50, 45, self.sprite:getWidth(), self.sprite:getHeight())
+    self.animation = anim8.newAnimation(g('1-2', 1), 0.1)
     self.offset = {
-        x = self.sprite:getWidth() / 2,
-        y = self.sprite:getHeight() / 2,
+        x = 50 / 2,
+        y = 45 / 2,
     }
 end
 
