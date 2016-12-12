04c85bbce5db159cf8df918392926d4e841de299
Diane Losardo <dlosardo@gmail.com>
2016-12-12 15:51:52 -0500

    adjusting music wip

--- src/entities/music.lua
+++ src/entities/music.lua
@@ -13,18 +13,28 @@ function Music:init()
             volume = 0.9
         }
     };
-
-    for key,value in pairs(self.themes) do
-        value.audio:setLooping(true)
-        value.audio:setVolume(value.volume)
-        value.audio:play()
-    end
+    self.calmDuration = self.themes.calm.audio:getDuration() - 0.13
+    self.intenseDuration = self.themes.intense.audio:getDuration()
+    print("calm: " .. self.themes.calm.audio:getDuration())
+    print("intense: " .. self.themes.intense.audio:getDuration())
+    self.themes.calm.audio:setVolume(0.9)
+    --self.themes.calm.audio:setLooping(true)
+    self.themes.calm.audio:play()
+    self.themes.calm.audio:seek(9) -- temp
+    --for key,value in pairs(self.themes) do
+    --    print(value)
+    --    --if value ==  then
+    --        --value.audio:setLooping(true)
+    --    value.audio:setVolume(value.volume)
+    --    value.audio:play()
+    --    --end
+    --end
     self.isMusic = true
 
-    self.transitionCooldown = 0;
-    self.transitionTime = 0;
-    self.transitionTheme = nil;
-    self.transitionVolume = 0;
+    self.transitionCooldown = 9
+    self.transitionTime = 0
+    self.transitionTheme = nil
+    self.transitionVolume = 0
 end
 
 function Music:transition(theme, time, volume)
--- src/systems/musicsystem.lua
+++ src/systems/musicsystem.lua
@@ -12,36 +12,41 @@ function MusicSystem:postProcess(dt)
 end
 
 function MusicSystem:process(e, dt)
-    if(e.transitionCooldown <  0) then
-        for key,value in pairs(e.themes) do
-            if(e.transitionTheme == key) then
-                value.volume = e.transitionVolume
-            else
-                value.volume = 0
-            end
-        end
+    --if(e.transitionCooldown <  0) then
+    --    for key,value in pairs(e.themes) do
+    --        if(e.transitionTheme == key) then
+    --            value.volume = e.transitionVolume
+    --        else
+    --            value.volume = 0
+    --        end
+    --    end
 
-        e.transitionCooldown = 0
+    --    e.transitionCooldown = 0
+    --end
+
+    --if(not (e.transitionCooldown == 0)) then
+    --    e.transitionCooldown = e.transitionCooldown - dt
+    --    local transitionPercent = 1 - ((e.transitionCooldown) / e.transitionTime);
+
+    --    for key,value in pairs(e.themes) do
+    --        local fromVolume = value.volume
+
+    --        local toVolume
+    --        if(e.transitionTheme == key) then
+    --            toVolume = e.transitionVolume
+    --        else
+    --            toVolume = 0
+    --        end
+
+    --        value.audio:setVolume(fromVolume * (1 - transitionPercent) + toVolume * transitionPercent)
+    --    end
+    --end
+    e.transitionCooldown = e.transitionCooldown + dt
+    if e.transitionCooldown >= e.calmDuration then -- time passed since our e.calmDuration second long song
+        e.themes.calm.audio:play() -- force song to replay
+        e.themes.calm.audio:seek(e.transitionCooldown - e.calmDuration, 'seconds') -- jump to position
+        e.transitionCooldown = e.transitionCooldown - e.calmDuration
     end
-
-    if(not (e.transitionCooldown == 0)) then
-        e.transitionCooldown = e.transitionCooldown - dt
-        local transitionPercent = 1 - ((e.transitionCooldown) / e.transitionTime);
-
-        for key,value in pairs(e.themes) do
-            local fromVolume = value.volume
-
-            local toVolume
-            if(e.transitionTheme == key) then
-                toVolume = e.transitionVolume
-            else
-                toVolume = 0
-            end
-
-            value.audio:setVolume(fromVolume * (1 - transitionPercent) + toVolume * transitionPercent)
-        end
-    end
-
 end
 
 return MusicSystem
