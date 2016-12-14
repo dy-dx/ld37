6355d82557b150f2982afd8f08f7b2c31e486006
Diane Losardo <dlosardo@gmail.com>
2016-12-14 00:48:52 -0500

    fixed text in cutscenes

--- src/systems/drawsystems/cutscenedrawsystem.lua
+++ src/systems/drawsystems/cutscenedrawsystem.lua
@@ -30,11 +30,6 @@ local getCurrentDialogue = function(e)
     end
 end
 
-local function wrapText(e, toPrint)
-    if string.len(toPrint) > e.maxTextWidth then
-        return Text:setf(toPrint, 3)
-    end
-end
 function CutsceneDrawSystem:process(e, dt)
     if not Global.isCutscene then return end
     e.timer:update(dt)
@@ -66,15 +61,28 @@ function CutsceneDrawSystem:process(e, dt)
     if not e.drawRestOfText then
         e.time = e.time + dt
         e.currentCharacter = math.floor((e.time * e.speed) / 1000)
-        local toPrint = string.sub(line, 0, e.currentCharacter) or ""
-        local newTextWidth, wrappedText = font:getWrap(toPrint, e.maxTextWidth)
+        local newTextWidth, wrappedText = font:getWrap(line, e.maxTextWidth)
+        local toPrint = {}
+        local ncharCounter = 0
+        for row, l in ipairs(wrappedText) do
+            local tmp_table = {}
+            for i = 1, #l do
+                local c = l:sub(i,i)
+                if ncharCounter < e.currentCharacter then
+                    table.insert(tmp_table, c)
+                else
+                    break
+                end
+                ncharCounter = ncharCounter + 1
+            end
+            table.insert(toPrint, tmp_table)
+        end
         local i = 1
-        lume.map(wrappedText, function(l)
+        lume.map(toPrint, function(l)
             local currentFont = love.graphics.newText(font, l)
             love.graphics.draw(currentFont, x, y + i * currentFont:getHeight())
             i = i + 1
         end)
-        --love.graphics.draw(wrappedText, x, y)
         e.currentCharacter = e.currentCharacter + 1
     else
         local newTextWidth, wrappedText = font:getWrap(line, e.maxTextWidth)
